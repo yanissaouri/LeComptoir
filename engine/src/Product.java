6e34ba2deb7ad48d61bbce2c9236e2e5a6267e05
yanissaouri <yaniss.aouri@laplateforme.io>
2026-09-22 12:20:56 +0200

    finished structure of product

---
+++ engine/src/Product.java
@@ -0,0 +1,13 @@
+public class Product {
+    int reference;
+    String name;
+    float price;
+    String category;
+
+    public Product(int reference, String name, int price,String category){
+        this.reference = reference;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+}
--- engine/src/Products.java
+++
@@ -1,9 +0,0 @@
-public class Products {
-    String name = "";
-    int price = 0;
-    String category = "";
-
-    public createProduct(String name, int price,String category){
-        return null;
-    }
-}
