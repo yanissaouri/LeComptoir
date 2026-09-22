7dc68f0f580cf05be53a4b4501f857007b8ab2ee
yanissaouri <yaniss.aouri@laplateforme.io>
2026-09-22 11:23:23 +0200

    feat: added Products class

---
+++ engine/src/Main.java
@@ -0,0 +1,19 @@
+
+void main() {
+
+    String products = "";
+    String drinks = "";
+    int amount = 0;
+    float tva = 0.2f;
+    int rewardsCard = 0;
+    
+
+
+
+    IO.println(String.format("Hello and welcome!"));
+
+    for (int i = 1; i <= 5; i++) {
+
+        IO.println("i = " + i);
+    }
+}
