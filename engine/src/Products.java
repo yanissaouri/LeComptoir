708d28180424ea94cd44c7b5e1088de7fe93c91d
rayanebenoudia <rayane.benoudia@laplateforme.io>
2026-09-22 12:21:57 +0200

    feat:cart

---
+++ engine/src/Products.java
@@ -0,0 +1,9 @@
+public class Products {
+    String name = "";
+    int price = 0;
+    String category = "";
+
+    public createProduct(String name, int price,String category){
+        return null;
+    }
+}
