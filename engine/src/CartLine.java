1dc95fb4e6d15425618969f2c957111b38a5aea7
yanissaouri <yaniss.aouri@laplateforme.io>
2026-09-22 12:36:20 +0200

    feat: added cartline class and its constructor

---
+++ engine/src/CartLine.java
@@ -0,0 +1,9 @@
+public class CartLine {
+    Product product;
+    int amount;
+
+    public CartLine(Product product, int amount){
+        this.product = product;
+        this.amount = amount;
+    }
+}
